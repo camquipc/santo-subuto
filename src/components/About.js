dfecce18695859a21d116edc35ee155ba045b1c5
Francisco Campos <camqui2011@gmail.com>
2023-10-15 13:49:37 -0400

    Cambios en el parrafo

--- src/components/About.js
+++ src/components/About.js
@@ -1,115 +1,131 @@
-import React from 'react'
+import React from "react";
 
 export const About = () => {
-    return (
-        <>
-            <section id="biografia" className="about">
-                <div className="container" data-aos="fade-up">
+  return (
+    <>
+      <section id="biografia" className="about">
+        <div className="container" data-aos="fade-up">
+          <div className="section-title">
+            <h2>Biografía</h2>
+          </div>
 
-                    <div className="section-title">
-                        <h2>Biografía</h2>
-                    </div>
+          <div className="row">
+            <div className="col-lg-5">
+              <img src="img/don5.jpg" className="img-thumbnail" alt="" />
+            </div>
+            <div className="col-lg-7 pt-4 pt-lg-0 content text-justificado">
+              <h4 className="about-h4">¿Quién fue el Padre Jorge Bissoni?</h4>
+              <p>
+                El Padre Jorge Bissoni fue un destacado sacerdote nacido el 12
+                de mayo de 1940 en un pequeño pueblo del sur de Italia. A pesar
+                de haber vivido una infancia marcada por las devastaciones de la
+                Segunda Guerra Mundial, sintió desde temprana edad su vocación
+                al sacerdocio y se unió al seminario menor a los 10 años, fue
+                ordenado sacerdote en 28 de junio de 1959, a la temprana edad de
+                19 años.
+              </p>
 
-                    <div className="row">
-                        <div className="col-lg-5">
-                            <img src="img/don5.jpg" className="img-thumbnail" alt="" />
-                        </div>
-                        <div className="col-lg-7 pt-4 pt-lg-0 content text-justificado">
-                            <h4 className='about-h4'>¿Quién fue el Padre Jorge Bissoni?</h4>
-                            <p>
-                                El Padre Jorge Bissoni fue un destacado sacerdote nacido el 12 de mayo de 1940 en un pequeño pueblo del sur de Italia.
-                                A pesar de haber vivido una infancia marcada por las devastaciones de la Segunda Guerra Mundial,
-                                sintió desde temprana edad su vocación al sacerdocio y se unió al seminario menor a los 10 años,
-                                fue ordenado sacerdote en 28 de junio de 1959, a la temprana edad de 19 años.
-                            </p>
+              <p>
+                Su devoción lo llevó a convertirse en misionero, y se dirigió a
+                América Latina, específicamente a Cartago, Colombia, donde
+                comenzó su labor como sacerdote. Tras 12 años de servicio en esa
+                comunidad, fue enviado a continuar su misión en Venezuela,
+                estableciéndose en la parroquia 10 de Marzo en Pariata, La
+                Guaira. Durante su tiempo en esta parroquia, promovió la
+                devoción a la Virgen María, la ayuda a los necesitados fundando
+                el dispensario de Pariata, que ha servido a la comunidad desde
+                sus inicios.
+              </p>
 
-                            <p>
-                                Su devoción lo llevó a convertirse en misionero, y se dirigió a América Latina,
-                                específicamente a Cartago, Colombia, donde comenzó su labor como sacerdote.
-                                Tras 12 años de servicio en esa comunidad, fue enviado a continuar su misión en Venezuela,
-                                estableciéndose en la parroquia 10 de Marzo en Pariata, La Guaira. Durante su tiempo en esta parroquia,
-                                promovió la devoción a la Virgen María, la ayuda a los necesitados fundando el dispensario de Pariata,
-                                que ha servido a la comunidad desde sus inicios.
-                            </p>
+              <p>
+                Luego de 14 años en la Parroquia 10 de Marzo, fue llamado a
+                servir en la Diócesis de Carúpano, específicamente en la
+                Parroquia Inmaculada Concepción de Güira de la Costa, donde
+                permaneció durante 11 años, llevando el evangelio y
+                estableciendo el Centro de Formación San Antonio (CFSA), que
+                brindaba educación a jóvenes y adultos.
+              </p>
+            </div>
 
-                            <p>
-                                Luego de 14 años en la Parroquia 10 de Marzo,
-                                fue llamado a servir en la Diócesis de Carúpano,
-                                específicamente en la Parroquia Inmaculada Concepción de Güira de la Costa,
-                                donde permaneció durante 11 años, llevando el evangelio y estableciendo el Centro de Formación San Antonio (CFSA),
-                                que brindaba educación a jóvenes y adultos.
-                            </p>
-                        </div>
+            <div className="col-lg-7 pt-3 pt-lg-0 content mt-2 text-justificado">
+              <p>
+                Posteriormente, continuó su labor misionera en la parroquia San
+                Rafael Arcángel de Playa Grande, ofreciendo apoyo espiritual y
+                su ayuda a los más vulnerables, incluyendo a "Los Predilecto del
+                Señor", un proyecto que llevó a cabo en cada parroquia donde
+                prestó servicio como sacerdote.
+              </p>
 
-                        <div className='col-lg-7 pt-3 pt-lg-0 content mt-2 text-justificado'>
+              <p>
+                Durante sus 23 años de servicio en la Diócesis de Carúpano, el
+                Padre Jorge Bissoni promovió la realización de numerosas obras,
+                entre las que se destaca la Casa de la Caridad Santa Ana, que en
+                sus inicios sirvió como refugio para los forasteros necesitados
+                de un techo mientras sus familiares estaban en centros de salud.
+              </p>
 
-                            <p>
-                                Posteriormente, continuó su labor misionera en la parroquia San Rafael Arcángel de Playa Grande,
-                                ofreciendo apoyo espiritual y ayuda a los más vulnerables,
-                                incluyendo a "Los Predilecto del Señor", un proyecto que llevó a cabo en cada parroquia donde prestó
-                                servicio como sacerdote.
-                            </p>
+              <p>
+                Su humildad y valentía lo llevaron a difundir el evangelio, del
+                cual estaba profundamente enamorado. A través de extensas
+                jornadas de oración, fomentando en su feligresía una devoción
+                por la Virgen María en sus advocaciones de Nuestra Señora del
+                Valle y Nuestra Señora de los Favores. Cada 8 de septiembre la
+                comunidad se reunía con alegría para escuchar la palabra de Dios
+                que él proclamaba en su Eucaristía.
+              </p>
 
-                            <p>
-                                Durante sus 23 años de servicio en la Diócesis de Carúpano,
-                                el Padre Jorge Bissoni promovió la realización de numerosas obras,
-                                entre las que se destaca la Casa de la Caridad Santa Ana, que en sus inicios  sirvió como refugio para los
-                                forasteros necesitados de un techo mientras sus familiares estaban en centros de salud.
-                            </p>
+              <p>
+                Sin embargo, su trágico final llegó de manera abrupta el 17 de
+                septiembre de 2023, en un fatídico accidente de tránsito, en la
+                carretera nacional Carúpano - Cumaná, sector Pozo Colorado,
+                mientras iba a la comunidad de Guiria de la Playa a oficiar una
+                Santa Eucaristía. Su luz terrenal se apagó, dejando a quienes lo
+                conocieron con un profundo lamento y un vacío que solo el
+                recuerdo de su inquebrantable fe y dedicación puede llenar.
+              </p>
 
-                            <p>
-                                Su humildad y valentía lo llevaron a difundir el evangelio,
-                                del cual estaba profundamente enamorado. A través de extensas jornadas de oración
-                                fomentó en su feligresía la devoción a la Virgen María en sus advocaciones de Nuestra Señora del Valle
-                                y Nuestra Señora de los Favores. cada 8 de septiembre la  comunidad se reunia  con alegría
-                                para escuchar la palabra de Dios que él proclamaba en su Eucaristía.
-                            </p>
+              <p>
+                El Padre Jorge Bissoni dejó un legado imborrable que perdura en
+                todas las comunidades a las que dedicó su vida. Su obra
+                trasciende el tiempo y el espacio, impactando positivamente a
+                quienes tuvieron la fortuna de cruzar su camino. Fue, sin duda,
+                un hombre de Dios en la tierra, un ejemplo vivo de humildad y
+                servicio desinteresado. Como nos lo recuerda el Evangelio de
+                Mateo 6:20,{" "}
+                <span className="fst-italic">
+                  <b>
+                    "No acumulen para sí tesoros en la tierra, donde la polilla
+                    y el óxido destruyen, y donde los ladrones se meten a robar.
+                    Más bien, acumulen para sí tesoros en el cielo, donde ni la
+                    polilla ni el óxido carcomen, ni los ladrones se meten a
+                    robar".
+                  </b>
+                </span>
+              </p>
 
-                            <p>
-                                Sin embargo, su trágico final llegó de manera abrupta el 17 de septiembre de 2023,
-                                en un fatídico accidente de tránsito, en la carretera nacional Carúpano - Cumaná,
-                                sector Pozo Colorado, mientras iba a la comunidad de Guiria de la Playa a oficiar una Santa Eucaristía.
-                                Su luz terrenal se apagó, dejando a quienes lo conocieron con un profundo lamento
-                                y un vacío que solo el recuerdo de su inquebrantable fe y dedicación puede llenar.
+              <p>
+                Sus tesoros no se acumularon en la tierra, sino en los corazones
+                de aquellos que lo conocieron y en el cielo, donde su obra
+                eterna resplandece como una luz que nunca se apagará.
+              </p>
 
-                            </p>
+              <div className="cita-text">
+                <p className="fst-italic">
+                  “El cristiano esta hecho para dar, no para recibir.” <br />
+                  Padre Jorge Bissoni
+                </p>
+              </div>
+            </div>
 
-                            <p>
-                                El Padre Jorge Bissoni dejó un legado imborrable que perdura en todas las comunidades
-                                a las que dedicó su vida. Su obra trasciende el tiempo y el espacio,
-                                impactando positivamente a quienes tuvieron la fortuna de cruzar su camino. Fue,
-                                sin duda, un hombre de Dios en la tierra, un ejemplo vivo de humildad y servicio desinteresado.
-                                Como nos lo recuerda el Evangelio de Mateo 6:20, <span className='fst-italic'><b>"No
-                                    acumulen para sí tesoros en la tierra,
-                                    donde la polilla y el óxido destruyen, y donde los ladrones se meten a robar. Más bien,
-                                    acumulen para sí tesoros en el cielo, donde ni la polilla ni el óxido carcomen, ni
-                                    los ladrones se meten a robar".</b></span>
-                            </p>
-
-                            <p>
-                                Sus tesoros no se acumularon en la tierra, sino en los corazones de aquellos que lo conocieron
-                                y en el cielo, donde su obra eterna resplandece como una luz que nunca se apagará.
-                            </p>
-
-                            <div className='cita-text'>
-                                <p className="fst-italic">
-                                    “El cristiano esta hecho para dar, no para recibir.” <br />
-                                    Padre Jorge Bissoni
-                                </p>
-                            </div>
-                        </div>
-
-                        <div className="col-lg-5 mt-2">
-                            <img src="img/don26.jpeg" className="img-thumbnail" alt="" />
-                        </div>
-                    </div>
-
-                </div>
-            </section>
-        </>
-    )
-}
-
+            <div className="col-lg-5 mt-2">
+              <img src="img/don26.jpeg" className="img-thumbnail" alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
 /**
  * 
